37a5586b37e36fa8630c40dbeb60f37ceb05f80a
sniper-py <dzmbbs@qq.com>
2019-07-17 14:22:58 +0800

    2to3 urllib

--- seahub/avatar/templatetags/avatar_tags.py
+++ seahub/avatar/templatetags/avatar_tags.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2012-2016 Seafile Ltd.
 import logging
-import urllib
+import urllib.request, urllib.parse, urllib.error
 import hashlib
 
 from django import template
@@ -34,7 +34,7 @@ def avatar_url(user, size=AVATAR_DEFAULT_SIZE):
                 params['d'] = AVATAR_GRAVATAR_DEFAULT
             return "http://www.gravatar.com/avatar/%s/?%s" % (
                 hashlib.md5(user.email).hexdigest(),
-                urllib.urlencode(params))
+                urllib.parse.urlencode(params))
         else:
             url = get_default_avatar_url()
 
